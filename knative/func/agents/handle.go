2398941242335bd7c6e09f10118e4fb68322c453
jgnoguer <jgarciano@uoc.edu>
2025-05-05 20:57:11 +0200

    Try makedir

--- knative/func/agents/handle.go
+++ knative/func/agents/handle.go
@@ -26,7 +26,7 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("%q\n", dump)
 	fmt.Fprintf(w, "%q", dump)
 
-	dirError := os.MkdirAll("/data/hola/cara/cola", os.ModeDir)
+	dirError := os.Mkdir("/data/hola/", os.ModeDir)
 	if dirError != nil {
 		slog.Error("Fail to create directory" + dirError.Error())
 		w.WriteHeader(http.StatusInternalServerError)
