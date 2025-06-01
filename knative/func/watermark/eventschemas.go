f7a7f0cfe9ad42a01ddb538814e61212acd453c1
jgnoguer <jgarciano@uoc.edu>
2025-06-02 00:25:45 +0200

    Fix message

--- knative/func/watermark/eventschemas.go
+++ knative/func/watermark/eventschemas.go
@@ -3,7 +3,7 @@ package main
 // HelloWorld defines the Data of CloudEvent with type=dev.jgnoguer.knative.uoc.imageadded
 type ImageAdded struct {
 	// Msg holds the message from the event
-	Msg string `json:"msg,omitempty"`
+	MediaId string `json:"mediaId,omitempty"`
 }
 
 // HiFromKnative defines the Data of CloudEvent with type=dev.jgnoguer.knative.uoc.hifromknative
